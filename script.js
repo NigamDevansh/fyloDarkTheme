33901f7b1364442de9a03d0c52f3e270ded7d458
DevanshNigam <devanshn02@gmail.com>
2021-08-16 02:52:15 +0530

    preloader

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  const wrapper = document.querySelector(".loader-wrapper");
+  const loader = document.querySelector(".loader");
+  wrapper.style.display = "none";
+  wrapper.style.width = "0";
+};
